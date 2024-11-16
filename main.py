de258e2d7fd0c9c4bfab033c42a47e6c7185cc7a
Taisha (artem) <sunlesslove128@gmail.com>
2024-11-16 21:58:06 +0500

    Прописал овтет программы на неправльные вводимые данные, и не существующий акканут, начал прописывать окно пароля на девайсе, сделал возможность регистрации аккаунта

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import sys
 import sqlite3
 import traceback
+from dbm import error
 from sys import excepthook
 
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt6.QtCore import QPropertyAnimation, QPoint
+from PyQt6.QtCore import QPropertyAnimation, QPoint, QTimer
 from PyQt6.uic import loadUi
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.loadUi()
+
+    def loadUi(self):
+        uic.loadUi('registrationWindow.ui', self)
 
 
 class AuthorizationWindow(QWidget): # 920x562
@@ -24,6 +34,7 @@ class AuthorizationWindow(QWidget): # 920x562
         self.loginButton.clicked.connect(self.log_in) # Залогиниться
         self.registrationRegButton.clicked.connect(self.register) # Зарегистрироваться
 
+    # Анимации
     def open_register_frame(self):
         animation_login_frame = QPropertyAnimation(self.animationFrame, b"pos", self)
         animation_login_frame.setDuration(1000)
@@ -38,23 +49,25 @@ class AuthorizationWindow(QWidget): # 920x562
         animation_login_frame.setEndValue(QPoint(500, -500))
         animation_login_frame.start()
 
-    def register(self):
+    def register(self): # Функция регистрации
         name = self.nameRegEdit.text()
         surname = self.surnameRegEdit.text()
         patronymic = self.patronymicRegEdit.text()
         passport_details = self.passportDetailsRegEdit.text()
         age = self.ageRegEdit.text()
-
         if self.check_data(name, surname, patronymic, passport_details, age, who_called_check="registerFunc"):
             self.connect_db()
-
             sql = '''INSERT INTO users(name, surname, patronymic, passport_details, age)
                      VALUES (?, ?, ?, ?, ?);'''
             self.cursor.execute(sql, (name, surname, patronymic, passport_details, age))
+            self.open_login_frame()
             self.connect.commit()
             self.connect.close()
+        else:
+            self.errorLabelReg.setText("Данные для регистрации введены в неверном формате")
+            self.timer_error_text(self.errorLabelReg)
 
-    def log_in(self):
+    def log_in(self): # Функция логина
         name = self.userNameEdit.text()
         surname = self.userSurnameEdit.text()
         passport_details = self.passportDetailsEdit.text()
@@ -63,19 +76,39 @@ class AuthorizationWindow(QWidget): # 920x562
             sql = '''SELECT user_id
                      FROM users
                      WHERE name = ? AND surname = ? AND passport_details = ?'''
-            self.cursor.execute(sql, (name, surname, passport_details)).fetchall()
-            self.connect.close()
-            print("Заебись")
+            if self.cursor.execute(sql, (name, surname, passport_details)).fetchall():
+                # Прописать создание экземпляра класса, открывающего основное окно приложения + Прописать функцию "запомнить меня"
+                self.connect.close()
+            else:
+                self.errorLabel.setText("Аккаунта с такими данными не существует")
+                self.timer_error_text(self.errorLabel)
         else:
-            self.passportDetailsEdit.setPlaceholderText("Вы неверно ввели паспортные данные")
-            # Прописать что будет выводиться при неправильных данных
+            self.errorLabel.setText("Данные для входа введены в неверном формате")
+            self.timer_error_text(self.errorLabel)
 
     def check_data(self, *kwargs, who_called_check):
         if who_called_check == "loginFunc":
             passport_details = kwargs[2]
+            name = kwargs[0]
+            surname = kwargs[1]
             if len(passport_details) != 11:
                 return False
-            if passport_details.index(" ") != 4:
+            if " " not in passport_details and passport_details.index(" ") != 4:
+                return False
+            if len(name) == 0 or len(surname) == 0:
+                return False
+            return True
+        elif who_called_check == "registerFunc":
+            name = kwargs[0]
+            surname = kwargs[1]
+            patronymic = kwargs[2]
+            passport_details = kwargs[3]
+            age = kwargs[4]
+            if len(name) == 0 or len(surname) == 0 or len(patronymic) == 0:
+                return False
+            if " " not in passport_details and passport_details.index(" ") != 4:
+                return False
+            if not age.isdigit():
                 return False
             return True
 
@@ -83,14 +116,21 @@ class AuthorizationWindow(QWidget): # 920x562
         self.connect = sqlite3.connect("Bank")
         self.cursor = self.connect.cursor()
 
+    def timer_error_text(self, widget):
+        self.timer = QTimer(self)
+        self.timer.setSingleShot(True)  # Таймер срабатывает один раз
+        self.timer.timeout.connect(lambda: widget.clear())  # Используем лямбда-функцию для очистки текста
+        self.timer.start(3000)
 
-class MainWindow(QMainWindow):
+
+class DevicePasswordWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.loadUi()
 
     def loadUi(self):
-        uic.loadUi('registrationWindow.ui', self)
+        uic.loadUi("passwordDevice.ui", self)
+
 
 def excepthook(exc_type, exc_value, exc_tb):
     tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
@@ -100,6 +140,6 @@ def excepthook(exc_type, exc_value, exc_tb):
 if __name__ == '__main__':
     sys.excepthook = excepthook
     app = QApplication(sys.argv)
-    ex = AuthorizationWindow()
+    ex = DevicePasswordWindow()
     ex.show()
     sys.exit(app.exec())
